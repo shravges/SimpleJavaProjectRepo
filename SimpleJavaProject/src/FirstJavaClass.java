79f7bb7ff312eb90aafaf4d6fed1614885a70634
Shilps <Shilps@LAPTOP-AQ5CIB5M>
2021-04-14 14:48:11 +0530

    first commit

---
+++ SimpleJavaProject/src/FirstJavaClass.java
@@ -0,0 +1,13 @@
+
+public class FirstJavaClass {
+	public void Display()
+	{
+		System.out.println("This is simple java project demo");
+	}
+
+	public static void main(String[] args)
+	{
+		FirstJavaClass f1= new FirstJavaClass();
+		f1.Display();
+	}
+}
